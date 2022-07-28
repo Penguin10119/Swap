39c7aa1f81606966f906f82475d6964f85cc0b29
Penguin10119 <85607253+Penguin10119@users.noreply.github.com>
2022-07-28 12:17:12 +0530

    Create Stars.py

---
+++ Stars.py
@@ -0,0 +1,11 @@
+a = int(input("enter number of rows: "))
+for i in range(0, a):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print(" ")
+
+
+for i in range(a + 1, 0, -1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+    print(" ")
